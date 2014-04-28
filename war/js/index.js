ac96a51a6dd16c28f0743440962a0fcf40e6edc0
alearnaiz <alejandro.arnaiz.gonzalez@gmail.com>
2014-04-29 00:01:38 +0200

    Cambios en el cliente, ahora contiene todos los servicios

--- war/js/index.js
+++ war/js/index.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 	$("#botonCargaInicial").click(function(){
+		$("#grupo").html('');
 		var text = $("#textoCargaInicial").val();
 		if (!text){
 			alert("Datos vacios");
@@ -7,12 +8,15 @@ $(document).ready(function(){
 		else{
 			$.post("http://localhost:8888/api/carga", text, function(){
 				$("#textoCargaInicial").val('');
+			}).done(function(){
+				$("#grupo").append($("<p>Insertado Grupos Correctamente</p>"));
 			});
 			
 		}
 	});
 	
 	$("#botonGrupo").click(function(){
+	$("#grupo").html('');
 		var text = $("#textoGrupo").val();
 		if (!text){
 			alert("Datos vacios");
@@ -22,10 +26,102 @@ $(document).ready(function(){
 				
 				$("#grupo").empty();
 				$.each(data, function(index, value) {
-					$("#grupo").append($("<p>"+ index + ": " + value +"</p>"));
+					$("#grupo").append($("<p><b>"+ index + "</b>: " + value +"</p>"));
 				});
 				
 				$("#textoGrupo").val('');
+			});
+		}
+	});
+	
+	$("#botonGrupos").click(function(){
+		$("#grupo").html('');
+			$.getJSON("http://localhost:8888/api/grupos", function(data){
+				$.each(data, function(key,val){
+						$("#grupo").append($("<p>Nombre: "+ val.nombre +"<br/>Votos Positivos: "+val.votosPositivos+"<br/>Votos Negativos:"+val.votosNegativos+"</p>"));
+						
+				});
+				
+			});
+		
+	});
+	
+		
+	$("#botonGrupoBorrarT").click(function(){
+		
+			$("#grupo").html('');
+			$.ajax({
+				url: 'http://localhost:8888/api/grupos',
+				type: 'DELETE',
+				success: function(response) {
+						$("#grupo").append($("<p>Borrado correctamente todos los grupos</p>"));
+				},
+				error: function(response){
+						alert("Fallo al borrar los datos");
+					}
+			});
+		
+	});
+	
+	$("#botonGrupoBorrar").click(function(){
+		$("#grupo").html('');
+		var text = $("#textoGrupoBor").val();
+		if (!text){
+			alert("Datos vacios");
+		}
+		else{
+			$.ajax({
+				url: 'http://localhost:8888/api/grupos/'+text,
+				type: 'DELETE',
+				success: function(response) {
+						$("#grupo").append($("<p>Borrado correctamente el grupo "+text+"</p>"));
+						$("#textoGrupoBor").val('');
+				},
+				error: function(response){
+						alert("Fallo al borrar los datos");
+					}
+			});
+		}
+	});
+	
+	$("#botonGrupoInser").click(function(){
+		$("#grupo").html('');
+		var text = $("#textoGrupoInser").val();
+		if (!text){
+			alert("Datos vacios");
+		}
+		else{
+			$.post("http://localhost:8888/api/grupos", text, function(){
+				$("#textoGrupoInser").val('');
+			}).done(function(){
+				$("#grupo").append($("<p>Grupo Insertado Correctamente</p>"));
+			}).error(function(){
+				alert("Error al insertar datos");
+			});
+			
+		}
+	});
+	
+	$("#botonGrupoAct").click(function(){
+		$("#grupo").html('');
+		var text = $("#textoGrupoAct").val();
+		var data = $("#textoActualizar").val();
+		if (!text || !data){
+			alert("Datos vacios");
+		}
+		else{
+			$.ajax({
+				url: 'http://localhost:8888/api/grupos/'+text,
+				type: 'PUT',
+				data: data,
+				success: function(response) {
+						$("#grupo").append($("<p>Actualizado correctamente el grupo "+text+"</p>"));
+						$("#textoGrupoAct").val('');
+						 $("#textoActualizar").val('');
+				},
+				error: function(response){
+						alert("Fallo al actualizar los datos");
+					}
 			});
 		}
 	});
